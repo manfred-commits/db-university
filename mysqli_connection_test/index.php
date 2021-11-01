c68e78975de8c857db0fabfb4ff368a9f5da1be5
manfred-commits <manfredipiraino@gmail.com>
2021-11-01 20:10:31 +0100

    refactoring config

--- mysqli_connection_test/index.php
+++ mysqli_connection_test/index.php
@@ -1,12 +1,8 @@
 <?php
 
 // definition of constant variables for my sqli connection
-define('DB_SERVERNAME', 'localhost:3306');
-define('DB_USERNAME', 'root');
 
-// default mamp mysql, password
-define('DB_PASSWORD','root');
-define('DB_NAME', 'db_university');
+require_once __DIR__."/config.php";
 
 // connection through mysqli
 $conn=new mysqli(DB_SERVERNAME,DB_USERNAME,DB_PASSWORD,DB_NAME);
