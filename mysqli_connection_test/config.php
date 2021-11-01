8cbe321da743e93c5ba4ada043c096aa44a6525d
manfred-commits <manfredipiraino@gmail.com>
2021-11-01 20:10:20 +0100

    added designated php file,  connection config

---
+++ mysqli_connection_test/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// definition of constant variables for my sqli connection
+define('DB_SERVERNAME', 'localhost:3306');
+define('DB_USERNAME', 'root');
+
+// default mamp mysql, password
+define('DB_PASSWORD','root');
+define('DB_NAME', 'db_university');
